8ac20f6c3d701c2eed6b879f6896a14f21fa44bd
user <danilovsansan@mail.ru>
2023-12-04 12:23:28 +0300

    ДЗ 2/4 Инициализируйте (присвойте значение) переменную friend значением 19. Увеличьте значение переменной на 2, после чего поделите значение на 7.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 
-// задача номер 1 присвоить переменным значения и вывести на консоль
+        // задача номер 1 присвоить переменным значения и вывести на консоль
 public class Main {
     public static void main(String[] args) {
         var dog = 8.0;
@@ -10,7 +10,7 @@ public class Main {
         System.out.println("cat=" + cat);
         System.out.println("paper=" + paper);
 
-//  Задача номер 2 увеличить переменные на 4
+        //  Задача номер 2 увеличить переменные на 4
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
@@ -18,12 +18,7 @@ public class Main {
         System.out.println("dog=" + dog);
         System.out.println("cat=" + cat);
         System.out.println("paper=" + paper);
-//  Задача 3
-//Теперь нужно уменьшить значение каждой переменной:
-//
-// dog уменьшить на 3.5;
-// cat уменьшить на 1.6;
-// paper уменьшить на 7639
+
         dog = dog-3.5;
         cat = cat-1.6;
         paper = paper-7639;
@@ -31,6 +26,20 @@ public class Main {
         System.out.println("dog=" + dog);
         System.out.println("cat=" + cat);
         System.out.println("paper=" + paper);
+
+        //Задача 4
+        //Инициализируйте (присвойте значение) переменную friend значением 19.
+        //Увеличьте значение переменной на 2, после чего поделите значение на 7.
+        var friend=19;
+        System.out.println("Задача N4");
+        System.out.println(friend);
+        friend=friend*2;
+        System.out.println(friend);
+        friend=friend/7;
+        System.out.println(friend);
+
+
+
 
     }
 }
