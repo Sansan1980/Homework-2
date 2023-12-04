79f08015890c631e9e6d5e412990edc103a8c9fb
user <danilovsansan@mail.ru>
2023-12-04 12:18:43 +0300

    ДЗ 2/3 Теперь нужно уменьшить значение каждой переменной

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println("cat=" + cat);
         System.out.println("paper=" + paper);
 
-// Задача номер 2 увеличить переменные на 4
+//  Задача номер 2 увеличить переменные на 4
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
@@ -18,5 +18,19 @@ public class Main {
         System.out.println("dog=" + dog);
         System.out.println("cat=" + cat);
         System.out.println("paper=" + paper);
+//  Задача 3
+//Теперь нужно уменьшить значение каждой переменной:
+//
+// dog уменьшить на 3.5;
+// cat уменьшить на 1.6;
+// paper уменьшить на 7639
+        dog = dog-3.5;
+        cat = cat-1.6;
+        paper = paper-7639;
+        System.out.println("Задача N3");
+        System.out.println("dog=" + dog);
+        System.out.println("cat=" + cat);
+        System.out.println("paper=" + paper);
+
     }
 }
