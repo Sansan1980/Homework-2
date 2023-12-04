e64dc4196a5ab65c78a17ee9b496e5720a958c97
user <danilovsansan@mail.ru>
2023-12-04 12:14:02 +0300

    ДЗ 2/2

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,19 @@ public class Main {
     public static void main(String[] args) {
         var dog = 8.0;
         var cat = 3.6;
-        var paper =763789;
+        var paper = 763789;
         System.out.println("Задача N1");
-        System.out.println("dog="+dog);
-        System.out.println("cat="+cat);
-        System.out.println("paper="+paper);
+        System.out.println("dog=" + dog);
+        System.out.println("cat=" + cat);
+        System.out.println("paper=" + paper);
+
+// Задача номер 2 увеличить переменные на 4
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println("Задача N2");
+        System.out.println("dog=" + dog);
+        System.out.println("cat=" + cat);
+        System.out.println("paper=" + paper);
+    }
+}
