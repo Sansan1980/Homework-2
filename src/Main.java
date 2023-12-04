0df80e5aac94ac781be6583267223551cfe8f555
user <danilovsansan@mail.ru>
2023-12-04 12:25:16 +0300

    ДЗ 2/5 Инициализируйте переменную frog значением 3.5. Увеличьте переменную в 10 раз и поделите на 3.5. Добавьте к последнему значению переменной 4.

--- src/Main.java
+++ src/Main.java
@@ -38,6 +38,21 @@ public class Main {
         friend=friend/7;
         System.out.println(friend);
 
+        //Задача 5
+        //Инициализируйте переменную frog значением 3.5.
+        //
+        //Увеличьте переменную в 10 раз и поделите на 3.5. Добавьте к последнему значению переменной 4.
+
+        System.out.println("Задача N5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog *10;
+        System.out.println(frog);
+        frog = frog /3.5;
+        System.out.println(frog);
+        frog = frog +4;
+        System.out.println(frog);
+
 
 
 
